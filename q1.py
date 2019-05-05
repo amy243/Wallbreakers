3c5343892acf927f39b0385413d801e49f095f01
Amy <lightning.atlas@gmail.com>
2019-05-05 04:18:39 -0400

    Add files via upload

---
+++ q1.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def flipAndInvertImage(self, A):
+        """
+        :type A: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        for i in A:
+            for j in range(int(len(i)/2)):
+                a=i[j]
+                i[j]=i[len(i)-(j+1)]
+                i[len(i)-(j+1)]=a
+        
+        
+            for k in range(len(i)):
+                if i[k]==1:
+                    i[k]=0
+                elif i[k]==0:
+                    i[k]=1
+        return A
