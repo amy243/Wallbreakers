cdca53f2f2ec2aeacc2554f2032cc81bdb6df01b
Amy <lightning.atlas@gmail.com>
2019-05-06 00:11:40 -0400

    Add files via upload

---
+++ q4.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def selfDividingNumbers(self, left, right):
+        y=[]
+        for k in range(left,right+1):
+            x=[]
+            i=k
+            
+            y.append(k)
+            while i>=1:
+
+                x.append((i%10))
+                i=int(i/10)
+            z=0
+            for j in x:
+                if j==0 or k%j!=0:
+                    z+=1
+            if z>0:
+                y.pop()
+        return y
+
+
